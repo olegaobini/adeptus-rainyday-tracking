7e9cd2a1705a337ea56cba67ae2b853096fba4f5
Hardimic <mjh49@uw.edu>
2026-03-20 17:57:28 -0700

    Massive cleanup of legacy files
    Removed unneeded scripts, sensor configurations, etc.

--- src/+trackbench/+loader/loadConfig.m
+++
@@ -1,9 +0,0 @@
-function config = loadConfig(configName, varargin)
-% LOADCONFIG  Backward-compatible wrapper.
-%   Main branch expects trackbench.loader.loadConfig().
-%   Real implementation lives in trackbench.config.loadConfig().
-%
-% See also: trackbench.config.loadConfig
-
-    config = trackbench.config.loadConfig(configName, varargin{:});
-end
